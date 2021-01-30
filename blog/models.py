909367dadddcd6a89e5ea1d075f1d03a234f0ba1
kyu929 <j2mity@gmail.com>
2021-01-30 16:47:52 +0900

    Modified files in blog

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,3 @@
-from django.conf import settings
 from django.db import models
-from django.utils import timezone
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+# Create your models here.
